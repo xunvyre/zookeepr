c58f93faae1b430878faad1fcc2e9d7911490b76
xunvyre <victoria@thedavids.us>
2022-08-20 12:39:24 -0400

    updated port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { animals } = require('./data/animals');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArr)
@@ -47,8 +48,8 @@ app.get('/api/animals', (req, res) =>
     }
     res.json(results);
 });
-app.listen(3001, () =>
+app.listen(PORT, () =>
 {
-    console.log(`API server now on port 3001!`);
+    console.log(`API server now on port ${PORT}!`);
 });
 'http://localhost:3001/api/animals'
